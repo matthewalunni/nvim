b14801b954c77269b5da697361dabfe079ff5b08
matthewalunni <matthew.alunni@gmail.com>
2025-09-27 01:22:58 -0400

    Add dashboard config with git and gh cli, wordwrap, dashboard shortcuts, small fixes

--- lua/configs/snacks.lua
+++ lua/configs/snacks.lua
@@ -11,17 +11,77 @@ require("snacks").setup({
 		view = "mini", -- or "notify" if you use `nvim-notify`
 	},
 	dashboard = {
-		enabled = true,
+		preset = {
+			-- Used by the `keys` section to show keymaps.
+			-- Set your custom keymaps here.
+			-- When using a function, the `items` argument are the default keymaps.
+			---@type snacks.dashboard.Item[]
+			keys = {
+				{ icon = " ", key = "f", desc = "Find File", action = "<leader>ff" },
+				{ icon = " ", key = "s", desc = "Find Text", action = "<leader>fg" },
+				{ icon = "󰛔", key = "S", desc = "Find and Replace", action = "<leader>sg" },
+				{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+
+				{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+				{
+					icon = " ",
+					key = "c",
+					desc = "Config",
+					action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+				},
+				{ icon = "", key = "g", desc = "Git Actions", action = "<leader>lg" },
+
+				{ icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+				{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+			},
+		},
 		sections = {
 			{ section = "header" },
+			{ pane = 1, section = "keys", gap = 1, padding = 1 },
+			{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 			{
 				pane = 2,
-				section = "terminal",
-				cmd = "echo 'Welcome to Neovim!'",
-				height = 5,
+				icon = " ",
+				desc = "Browse Repo",
 				padding = 1,
+				key = "b",
+				action = function()
+					Snacks.gitbrowse()
+				end,
 			},
-			{ section = "keys", gap = 1, padding = 1 },
+			function()
+				local in_git = Snacks.git.get_root() ~= nil
+				local cmds = {
+					{
+						icon = " ",
+						title = "Git Status",
+						cmd = "git --no-pager diff --stat -B -M -C",
+						height = 10,
+					},
+					{
+						icon = " ",
+						title = "Open PRs",
+						cmd = "gh pr list -L 3",
+						key = "P",
+						action = function()
+							vim.fn.jobstart("gh pr list --web", { detach = true })
+						end,
+						height = 7,
+					},
+				}
+				return vim.tbl_map(function(cmd)
+					return vim.tbl_extend("force", {
+						pane = 2,
+						section = "terminal",
+						enabled = in_git,
+						padding = 1,
+						ttl = 5 * 60,
+						indent = 3,
+					}, cmd)
+				end, cmds)
+			end,
+
 			{ section = "startup" },
 		},
 	},
@@ -35,6 +95,7 @@ require("snacks").setup({
 		sources = {
 			explorer = {
 				hidden = true,
+				ignored = false,
 			},
 		},
 	},
--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,14 +1,21 @@
-local telescope = require('telescope')
-local actions = require('telescope.actions')
+local telescope = require("telescope")
+local actions = require("telescope.actions")
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-k>'] = actions.move_selection_previous,
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-      },
-    },
-  },
+	defaults = {
+		mappings = {
+			i = {
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+			ignored = false,
+		},
+	},
 })
+
